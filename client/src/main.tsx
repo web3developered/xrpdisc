44151d0cb65654219464a1afc332142ff84a01eb
Simplelord <01nextup@gmail.com>
2026-08-24 14:02:33 +0100

    Polyfill Buffer before wallet bundle startup

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { App } from "./App";
-import "./styles.css";
+import { Buffer } from "buffer";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const browserGlobal = globalThis as typeof globalThis & { Buffer?: typeof Buffer };
+browserGlobal.Buffer ??= Buffer;
+
+void import("./bootstrap");
